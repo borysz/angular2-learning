1de3ec979a6555031b8e966034f1c434a9151746
Norbert Boros <assessia@gmail.com>
2018-01-14 21:35:34 +0100

    Angular getting started chapter 1 Signed-off-by: Norbert Boros <assessia@gmail.com>

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+//import { HEROES } from '../mock-heroes'; //átkerült a hero service-be
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+
+  heroes: Hero[];
+  selectedHero: Hero;
+  /*hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
+  */
+
+  constructor(private heroService: HeroService, private messageService: MessageService) { }
+
+  ngOnInit() {    
+    this.getHeroes();
+  }
+
+  onSelect(hero: Hero): void {    
+    this.messageService.add('HeroService: '+ hero.name);
+    this.selectedHero = hero;
+  }
+
+  getHeroes(): void {
+    //this.heroes = this.heroService.getHeroes();
+    this.heroService.getHeroes()
+    .subscribe(heroes => this.heroes = heroes);
+  }
+
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.heroService.addHero({ name } as Hero)
+      .subscribe(hero => {
+        this.heroes.push(hero);
+      });
+  }
+
+  delete(hero: Hero): void{
+    this.heroes = this.heroes.filter(h => h !== hero);
+    this.heroService.deleteHero(hero).subscribe();
+  }
+}
